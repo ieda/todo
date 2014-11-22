4645d7f2d121b4b2d05e8f96e7d8d94b5423db98
Ryo IEDA <ieda@val.co.jp>
2014-11-22 14:25:06 +0900

    フィクスチャの値がダミーであることを分かりやすくした

--- todo/spec/lib/cli_spec.rb
+++ todo/spec/lib/cli_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Todo::Cli do
   describe '#add' do
-    let(:message) { 'add test' }
+    let(:message) { 'dummy' }
     subject { described_class.new.add(message) }
     it { should be true }
   end
