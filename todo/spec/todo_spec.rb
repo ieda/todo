75f1b366d5c19418dbe1b00a4501cd8c4e8e1d4e
joe-re <joe.tialtngo@gmail.com>
2014-11-22 14:17:32 +0900

    minitestを除去

--- todo/spec/todo_spec.rb
+++ todo/spec/todo_spec.rb
@@ -1,14 +1,1 @@
-require_relative "spec_helper"
-require_relative "../todo.rb"
 
-def app
-  Todo
-end
-
-describe Todo do
-  it "responds with a welcome message" do
-    get '/'
-
-    last_response.body.must_include 'Welcome to the Sinatra Template!'
-  end
-end
