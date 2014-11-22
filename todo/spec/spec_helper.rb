a0d67c0604e60132ddc7d8c4938604608542d07a
Ryo IEDA <ieda@val.co.jp>
2014-11-22 16:28:19 +0900

    add gem database_cleaner

--- todo/spec/spec_helper.rb
+++ todo/spec/spec_helper.rb
@@ -13,3 +13,9 @@ require 'find'
 %w{./config/initializers ./lib}.each do |load_path|
   Find.find(load_path) { |f| require f if f.match(/\.rb$/) }
 end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+end
